1e615c8171281142f96d0028e7479fadd754d2bb
Ag <github@assembledgoods.com>
2013-04-30 12:42:43 -0500

    add   helper 'spree/base' to mailers

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,3 +1,4 @@
 Spree::OrderMailer.class_eval do
+  helper 'spree/base'
   layout 'spree/layouts/email'  
 end
--- app/mailers/spree/user_mailer_decorator.rb
+++ app/mailers/spree/user_mailer_decorator.rb
@@ -1,3 +1,4 @@
 Spree::UserMailer.class_eval do
+  helper 'spree/base'
   layout 'spree/layouts/email'
 end
